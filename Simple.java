1add88f95fb3b9c6256e74ff8bab214de06ff4e0
Amar Thool <ar.thool27@gmail.com>
2020-12-10 12:59:29 +0530

    Update Simple.java

--- Simple.java
+++ Simple.java
@@ -1,7 +1,6 @@
-class Simple
-{
-public static void main(String args[])
-{
-System.out.println("welcome to the house of horror...")
-}
+public class Simple {
+    public static void main(String args[]) {
+      System.out.println("Welcom to the stages of horrors..");
+
+    }
 }
