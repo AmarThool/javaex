50383a6f4d8232bc30fca9918e91cbbd9909cfe3
Amar Thool <ar.thool27@gmail.com>
2020-12-10 13:26:15 +0530

    Update Simple.java

--- Simple.java
+++ Simple.java
@@ -1,6 +1,6 @@
 public class Simple {
     public static void main(String args[]) {
-      System.out.println("Welcom to the stages of horrors.....wow...");
+      System.out.println("Welcom to the stages of horrors.....wow...how are you doing ....");
 
     }
 }
